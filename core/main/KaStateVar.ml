c78521abaea713a5ac18477a9a3ee11160f86845
Sándor Bartha <barthasanyi@gmail.com>
2024-12-17 10:49:51 +0000

    Remove sequences, use an imperative counter instead of zip.

--- core/main/KaStateVar.ml
+++ core/main/KaStateVar.ml
@@ -26,13 +26,14 @@ let list_opt_of_list l =
 
 let combine_list_opts l1 l2 = (list_of_list_opt l1) @ (list_of_list_opt l2)
   
-let seq_to_map empty update i =
-    Seq.fold_left 
+let list_to_map empty update i =
+    List.fold_left 
       (fun m (k,v) -> update k (fun o -> list_opt_of_list (combine_list_opts (list_opt_of_list v) o)) m) 
       empty 
       i
 
 (* Slow intersection implementation for list of lists*)
+
 let bin_intersection l1 l2 = List.filter (fun e -> List.mem e l2) l1   
 let list_intersection (sets : 'a list list) : 'a list =
     match sets with
@@ -41,54 +42,48 @@ let list_intersection (sets : 'a list list) : 'a list =
 
 (* Cartesian product for StringMap of lists *)
 
-let cartesian_on_string_maps (string_map : string list StringMap.t) : (string StringMap.t) Seq.t =
+let cartesian_on_string_maps string_map =
   StringMap.fold
     (fun key vs partial_map_seq -> 
-      Seq.concat_map 
+      List.concat_map 
         (fun partial_map ->
-          Seq.map
+          List.map
             (fun v -> StringMap.add key v partial_map) 
-            (List.to_seq vs)
+            vs
         )
         partial_map_seq 
     ) 
     string_map 
-    (Seq.return StringMap.empty)
+    [StringMap.empty]
   
-(* Infinite list of integers *)
-let rec ints n : int Seq.t = fun () -> Seq.Cons (n, ints (n + 1))
-
 (* helpers end here *)
 
 (* Collecting possible states for each agent/site froom signatures.
    Building a string * string -> string list map,
    mapping agent name * site name -> list of states. *)
 
-let states_to_state_names (states : state list) : (string list) =
+let states_to_state_names states =
    List.filter_map 
     (function 
         | StateName name -> Some name 
         | StateVar _ -> None 
         | StateAny -> None) states
    
-let rec states_of_site_list (agent : string) (sites : 'a site list) =  
+let rec states_of_site_list agent sites =  
     match sites with
-    | [] -> 
-      Seq.empty
+    | [] -> []
     | (Port port::sites) -> 
-      Seq.cons 
+      List.cons 
         ((agent,fst port.port_name), states_to_state_names (List.map fst port.port_int))
         (states_of_site_list agent sites)
     | (Counter _ :: sites) -> states_of_site_list agent sites
 
-let rec collect_agent_sigs 
-  (parsing_instructions : parsing_instruction list) : 
-  (agent_port * (string list)) Seq.t =
+let rec collect_agent_sigs parsing_instructions =
     match parsing_instructions with
-    | [] -> Seq.empty
+    | [] -> []
     | (pi::pis) ->
       match pi with 
-      | SIG (Present ((agent,_),sites,_mods)) -> Seq.append (states_of_site_list agent sites) (collect_agent_sigs pis)
+      | SIG (Present ((agent,_),sites,_mods)) -> (states_of_site_list agent sites) @ (collect_agent_sigs pis)
       | SIG (Absent _) | 
         (TOKENSIG (_,_)|VOLSIG (_, _, _)|
         INIT ((_,_), _)|
@@ -103,37 +98,36 @@ let rec collect_agent_sigs
         CONFIG ((_,_), _)|
         RULE (_, (_,_))) -> collect_agent_sigs pis
 
-let create_agent_sig_map 
-  parsing_instructions =
-  collect_agent_sigs parsing_instructions |> seq_to_map APMap.empty APMap.update
+let create_agent_sig_map parsing_instructions =
+  collect_agent_sigs parsing_instructions |> list_to_map APMap.empty APMap.update
 
 (* Collect list of state vars for a rule, adding agent/site names. *)
 
 let states_to_state_vars states =
-  Seq.filter_map  
+  List.filter_map  
    (function 
       | StateName _ -> None
       | StateVar var -> Some var
-      | StateAny -> None) (List.to_seq states);;
+      | StateAny -> None) states ;;
 
 let state_vars_of_port agent_name port =
   let agent_port = (agent_name,fst port.port_name) in
-  Seq.map (fun sv -> (sv,[agent_port])) (states_to_state_vars (List.map fst port.port_int))
+  List.map (fun sv -> (sv,[agent_port])) (states_to_state_vars (List.map fst port.port_int))
 
 let state_vars_of_site agent_name site =
     match site with
     | Port port -> state_vars_of_port agent_name port
-    | Counter _ -> Seq.empty
+    | Counter _ -> []
 
 let state_vars_of_agent agent =
      match agent with
-     | Present ((agent_name,_),site_list,_) -> Seq.concat (Seq.map (state_vars_of_site agent_name) (List.to_seq site_list))
-     | Absent _ -> Seq.empty
+     | Present ((agent_name,_),site_list,_) -> List.concat (List.map (state_vars_of_site agent_name) site_list)
+     | Absent _ -> []
 
 let state_vars_of_rule rule =
     match rule.rewrite with
     | Edit _ -> raise Unsupported_language_construct
-    | Arrow arrow -> Seq.concat (List.to_seq (List.map state_vars_of_agent (List.concat (arrow.lhs @ arrow.rhs))))
+    | Arrow arrow -> List.concat_map state_vars_of_agent (List.concat (arrow.lhs @ arrow.rhs))
   
 let subst_on_rule rule substs =
   let subst_on_state state =
@@ -171,13 +165,13 @@ let subst_on_rule rule substs =
   {rule with rewrite = subst_on_rule_content rule.rewrite}  
 let expand_rule ap_states_map rule =
   let var_aps_map : agent_port list StringMap.t = 
-      state_vars_of_rule rule |> seq_to_map StringMap.empty StringMap.update in
+      state_vars_of_rule rule |> list_to_map StringMap.empty StringMap.update in
   let state_var_to_states = StringMap.map (fun ap_list ->
       list_intersection 
           (List.map (fun ap -> list_of_list_opt (APMap.find_opt ap ap_states_map)) ap_list)
     ) var_aps_map in
-  let binding_seq = cartesian_on_string_maps state_var_to_states in
-  Seq.map (subst_on_rule rule) binding_seq
+  let bindings_list = cartesian_on_string_maps state_var_to_states in
+  List.map (subst_on_rule rule) bindings_list
   
 let expand_parsing_instruction ap_map pi =  
     match pi with 
@@ -195,9 +189,13 @@ let expand_parsing_instruction ap_map pi =
               match name with
               | None -> None
               | Some (name,loc) -> Some (name ^ "_" ^ (string_of_int ix),loc) in
-         List.of_seq (Seq.map 
-            (fun (ix,rule) -> RULE (convert_name name ix,(rule,loc)))
-            (Seq.zip (ints 1) (expand_rule ap_map rule))) ;;
+         List.map 
+            ( let counter = ref 0 in
+              fun rule ->
+                counter := !counter + 1 ;
+                RULE (convert_name name !counter,(rule,loc))
+            )
+            (expand_rule ap_map rule) ;;
                   
 (* let _ = print_endline "Expanding site variables." in
 let _ = print_endline "1. Parsing" in *)
